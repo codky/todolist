f976a1cf815e8caa8c0e7449e17788566fa960af
minseok kyeon <msky0912@gmail.com>
2024-12-24 17:03:49 +0900

    feature : 페이지네이션을 위한 대량 테스트 데이터 추가
     - layout 적용시 thymeleaf 적용이 안되서 잠시 적용 보류

--- src/test/java/com/example/demo/TodolistApplicationTests.java
+++ src/test/java/com/example/demo/TodolistApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ class TodolistApplicationTests {
 	private TodoRepository todoRepository;
 	
 	@Test
+	@Disabled
 	void testJpa() {
 		ToDoEntity todo1 = new ToDoEntity();
 		todo1.setContent("밥먹기");
@@ -21,6 +23,16 @@ class TodolistApplicationTests {
 		todo2.setContent("todolist 시작하기");
 		todo2.setCompleted(Boolean.FALSE);
 		this.todoRepository.save(todo2);
+	}
+	
+	@Test
+	void testData() {
+		for(int i=1; i<=300; i++) {
+			ToDoEntity todo1 = new ToDoEntity();
+			todo1.setContent("테스트 데이터입니다. ["+i+"]");
+			todo1.setCompleted(Boolean.TRUE);
+			this.todoRepository.save(todo1);
+		}
 	}
 
 }
