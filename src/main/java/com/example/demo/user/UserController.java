e969c9458a41574dc5edc401eaeaeabc5ea8ab60
minseok kyeon <msky0912@gmail.com>
2024-12-26 10:43:31 +0900

    feature : 회원가입 기능 추가
     - 회원가입 엔티티 생성(SiteUser)
     - User 레포지터리와 서비스 생성
     - Password Encoder 객체를 빈으로 등록(SecurityConfig)
     - 회원가입 폼 생성(UserCreateForm)
     - 회원가입 컨트롤러 생성(UserController)
     - 유효성검사를 위해 spring-boot-starter-validation 의존성 추가
     - 회원가입 화면 구성 : 템플릿 생성(signup_form.html)
     - 내비게이션바에 회원가입 링크 추가(@{/user/signup})

---
+++ src/main/java/com/example/demo/user/UserController.java
@@ -0,0 +1,40 @@
+package com.example.demo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	private final UserService userService;
+	
+	@GetMapping("/signup") // URL 이 GET요청이면 회원가입을 위한 템플릿을 렌더링
+	public String signup(UserCreateForm userCreateForm) {
+		return "signup_form";
+	}
+	
+	@PostMapping("/signup") // URL 이 POST요청이면 회원가입을 진행
+	public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "signup_form";
+		}
+		
+		if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) { // password1, password2 검증 조건
+			bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
+			return "signup_form";
+		}
+		
+		userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+		
+		return "redirect:/";
+	}
+
+}
---
+++ src/main/java/com/example/demo/user/UserService.java
@@ -0,0 +1,23 @@
+package com.example.demo.user;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+	
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder; // SecurityConfig에서 빈으로 등록한 Password Encoder객체를 주입받아 사용
+	
+	public SiteUser create(String username, String email, String password) {
+		SiteUser user = new SiteUser();
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setPassword(passwordEncoder.encode(password));
+		this.userRepository.save(user);
+		return user;
+	}
+}
